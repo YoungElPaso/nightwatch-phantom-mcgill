ff71f89b2dece6b822c8bcfba7c1f21fe9469778
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2016-04-23 23:34:48 -0400

    added a test that requires authentication, cant be run without u/pw combo and is excluded by default via nightwatch config

---
+++ tests/auth-blockWidthTest.js
@@ -0,0 +1,55 @@
+// Loads the contrast package.
+var contrast = require('get-contrast');
+
+// Loads the rgb package.
+var rgb = require('rgb');
+
+// A simple test for contrasting colors from two elements in the DOM.
+module.exports = {
+  'Block Width Test': function(client) {
+
+    // For this test, we need to go to mobile resolution on the client.
+    client.resizeWindow(400, 800);
+
+    // TODO: document this a bit better.
+    // Ideally this would be temporary or encrypted. Could be an include from
+    // NPM? or command line?
+    //console.log('globals? ', client.globals.authenticationPw);
+
+    // TODO: this should be conditioned, and a tag should be added to tests that
+    // require authentication, since by definition they can't be run in travis
+    // build and so will always fail. travis should build and run w/ passing
+    // tests.
+
+    // Go to a WMS page and login. TODO: add url to command-line options.
+    client.url('https://qa.ccs.mcgill.ca/samples/user/login')
+    .pause(300)
+    .setValue('#edit-name--2', client.globals.authentication.userName)
+    .setValue('#edit-pass--2', client.globals.authentication.pW)
+    .click('#edit-submit--2')
+    .pause(100);
+
+    // Go to the WMS page to see the blocks.
+    client.url('https://qa.ccs.mcgill.ca/samples/blocks/block-layouts')
+    .pause(300);
+
+    // Get a block we want and get the (NB: *computed*) css value for width.
+    var blockWidth;
+    client.getCssProperty('div.block.third-width',
+    'width', function(result) {
+      blockWidth = result.value;
+    });
+
+    // Perform allows us to run more complicated functions on the values we got.
+    client.perform(function(client, done) {
+
+      // Do some simple checks on the block values, validate they are strings.
+      client.assert.equal(typeof blockWidth, 'string');
+      // Assert the value of the width test.
+      client.assert.equal(blockWidth, '400px');
+
+      // Tell the client to end.
+      done();
+    });
+  }
+};
