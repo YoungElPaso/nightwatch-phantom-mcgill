ea2930d84f4ee3e87f1650d0d12c8ec500b5dc3d
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2016-05-03 13:11:46 -0400

    added better css selectors for finding the user/pw and submit elements and added better default and arguments for baseUrl (can be set in command line, test or resort to default now)

--- includes/globals.js
+++ includes/globals.js
@@ -1,5 +1,5 @@
 module.exports = {
-  doLogin: function(client) {
+  doLogin: function(client, url) {
     console.log('Doing login dance!');
     // TODO: this should be a function or a tag should be added to tests that
     // require authentication, since by definition they can't be run in travis
@@ -13,12 +13,19 @@ module.exports = {
       process.exit();
     }
 
-    // Go to a WMS page and login. TODO: add url to command-line options.
-    client.url('https://qa.ccs.mcgill.ca/samples/user/login')
+    // Go to a WMS page and login.
+    // Default url is a QA site...ideally this changes or is more stable.
+    var newUrl = url || 'https://qa.ccs.mcgill.ca/samples';
+    if (client.globals.baseUrl) {
+      newUrl = client.globals.baseUrl;
+    }
+    newUrl = newUrl + '/user/login';
+    console.log('Logging into...', newUrl);
+    client.url(newUrl)
     .pause(200)
-    .setValue('#edit-name--2', client.globals.authentication.userName)
-    .setValue('#edit-pass--2', client.globals.authentication.pW)
-    .click('#edit-submit--2')
+    .setValue('input[name="name"]', client.globals.authentication.userName)
+    .setValue('input[name="pass"]', client.globals.authentication.pW)
+    .click('input[value="Log in"]')
     .pause(100);
   }
 };
